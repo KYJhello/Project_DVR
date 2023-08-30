6f6794033f93d727dffa9a3e84c1514fecfd43c6
김용준 <kykyj0121@gmail.com>
2023-08-30 15:50:01 +0900

    feat. Same Commit

--- Assets/KIM/TestScripts.cs
+++ Assets/KIM/TestScripts.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TestScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rb;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(FishMoveRoutine());
+    }
+    float forceGravity = 0.01f;
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector3.down * forceGravity);
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FishMoveRoutine()
     {
-        
+        while (true)
+        {
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            yield return new WaitForSeconds(1f);
+        }
     }
+
 }
