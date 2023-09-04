ad6a29a07ad27da1d9ea94ae6196229e0af21ca7
안다비 안다비 <dksekqkr1234@naver.com>
2023-09-04 15:36:25 +0900

    feat : GameSetting 추가

---
+++ Assets/AHN/Scripts/Customer/CustomerSqawnManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AHN
+{
+    public class CustomerSqawnManager : MonoBehaviour
+    {
+        public void CustomerSqawn()
+        {
+            // 1. 오브젝트풀로 Customer 생성 -> 코루틴 이용.
+            // GameObject hitEffect = GameManager.Pool.Get(hitParticle, hit.point, Quaternion.LookRotation(hit.normal), hit.transform);
+            // 위에 이런식으로 함
+
+        }
+    }
+}
