f4e434ca8e2a9b11d2b3207f5190f70bcb8f2033
YongJoon_Kim <be2163@korea.ac.kr>
2023-09-09 16:23:54 +0900

    feat. Create StoreFish and KIM_FIshTank

---
+++ Assets/KIM/Scripts/KIM_FishTank.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KIM
+{
+    public class KIM_FishTank : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
