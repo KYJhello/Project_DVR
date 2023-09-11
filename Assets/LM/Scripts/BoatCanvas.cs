9a2052a7658b8aaac1316d1fb488e4dac7f4a4ea
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-09-11 19:49:27 +0900

    feat : boat move
    보트 움직임과 ui추가 및 수정
    ui프로젝터가 게이밍하게 번쩍이게 변경
    보트 내부에 텔레포트 앵커 하나 추가
    보트 내부에 스피어 소켓들 추가
    스피어 소켓에 리필 추가
    스피어 관련 코드 및 프리팹 약간 수정

---
+++ Assets/LM/Scripts/BoatCanvas.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LM
+{
+    public class BoatCanvas : BaseUI
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+
+    }
+}
