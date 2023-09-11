4c476c4387741bd41e89f940ca8d3c3e15715fc7
김용준 <kykyj0121@gmail.com>
2023-09-11 17:46:56 +0900

    feat. Create Prefabs

--- Assets/KIM/Scripts/KIM_FishTank.cs
+++ Assets/KIM/Scripts/KIM_FishTank.cs
@@ -8,24 +8,23 @@ namespace KIM
     {
         private List<Dictionary<string, string>> fishList = new List<Dictionary<string, string>>();
 
-        private void Awake()
-        {
-        }
-        private void CreateStoreFishes()
-        {
-            foreach (Dictionary<string, string> fishInfo in fishList)
-            {
-                GameObject go = GameManager.Resource.Instantiate<GameObject>("KIM_Prefabs/StoreFish", transform.position + Vector3.up, Quaternion.identity);
-                go.GetComponent<StoreFish>().GetFishInfo(fishInfo);
-            }
-        }
+        //private void CreateStoreFishes()
+        //{
+        //    foreach (Dictionary<string, string> fishInfo in fishList)
+        //    {
+        //        GameObject go = GameManager.Resource.Instantiate<GameObject>("KIM_Prefabs/StoreFish", transform.position + Vector3.up, Quaternion.identity);
+        //        go.GetComponent<StoreFish>().GetFishInfo(fishInfo);
+        //    }
+        //    gameObject.GetComponent<BoxCollider>().enabled = true;
+        //}
 
         private void OnTriggerEnter(Collider other)
         {
             // 만약 충돌한 물체의 레이어가 피쉬박스라면
             if(other.gameObject.layer == 15)
             {
-                if(other.gameObject.GetComponent<FishBox>().GetFishDicList().Count == 0)
+                //gameObject.GetComponent<BoxCollider>().enabled = false;
+                if(other.gameObject.GetComponent<FishBox>().GetFishDicList().Count == 0 )
                 {
                     return;
                 }
@@ -33,9 +32,12 @@ namespace KIM
                 foreach(Dictionary<string,string> fishInfo in other.gameObject.GetComponent<FishBox>().GetFishDicList())
                 {
                     fishList.Add(fishInfo);
+                    GameObject go = GameManager.Resource.Instantiate<GameObject>("KIM_Prefabs/StoreFish", transform.position + Vector3.up, Quaternion.identity);
+                    go.GetComponent<StoreFish>().GetFishInfo(fishInfo);
                 }
-                CreateStoreFishes();
+                //CreateStoreFishes();
             }
         }
+        
     }
 }
