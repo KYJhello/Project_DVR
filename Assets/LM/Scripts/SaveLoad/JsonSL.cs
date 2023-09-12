25c9f2146ba77b86f8309ff654c721a40c9d8311
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-09-12 21:21:13 +0900

    feat : Save&Load
    저장 및 로드 일부 구현
    사운드조절 구현
    프리팹들 추가
    발판 프리팹 이름 수정
    기타 자잘한 수정

---
+++ Assets/LM/Scripts/SaveLoad/JsonSL.cs
@@ -0,0 +1,83 @@
+using KIM;
+using LM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JsonSL : MonoBehaviour
+{
+    [SerializeField] Vector3 homePos;
+    [SerializeField] Vector3 seaPos;
+
+    [Serializable]
+    public struct SaveList
+    {
+        public List<SaveData> saves;
+    }
+
+    [Serializable]
+    public class SaveData
+    {
+        public int day;
+        public bool isHome;
+        public int money;
+        public List<SaveFishData> fishTank;
+        public float repute;
+        public float curWeight;
+        public int harpoonLevel;
+        public List<SaveFishData> fishBox;
+
+        public float masterVolume;
+        public float SFXVolume;
+        public float BGMVolume;
+    }
+    public class SaveFishData
+    {
+
+    }
+
+    public void Save()
+    {
+        SaveData save = new SaveData();
+        // save.day =
+        // save.isHome =
+        // save.money =
+        // save.fishTank
+        // save.repute = 
+        save.curWeight = FindObjectOfType<Diver>().CurWeight;
+        save.harpoonLevel = FindObjectOfType<HarpoonGun>().Level;
+        // save.fishBox = 
+
+        SettingUI ui = FindObjectOfType<SettingUI>();
+        // save.masterVolume = ui.
+
+        string json = JsonUtility.ToJson(save, true);
+        string path = Path.Combine(Application.dataPath, "save.json");
+        File.WriteAllText(path, json);
+    }
+    public void Load() 
+    {
+        // 화면 가리기
+        string path = Path.Combine(Application.dataPath, "save.json");
+        string loadJson = File.ReadAllText(path);
+        SaveData save = JsonUtility.FromJson<SaveData>(loadJson);
+        // save.day =
+        if(save.isHome)
+        {
+            FindObjectOfType<CharacterController>().Move(homePos);
+            // 배도 집으로
+        }
+        else
+        {
+            FindObjectOfType<CharacterController>().Move(seaPos);
+            // 배도 바다로
+        }
+        // save.money =
+        // save.fishTank
+        // save.repute = 
+        FindObjectOfType<Diver>().CurWeight = save.curWeight;
+        FindObjectOfType<HarpoonGun>().Level = save.harpoonLevel;
+        // save.fishBox = 
+    }
+}
