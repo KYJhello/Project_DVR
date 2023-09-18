509093378cf7b88163f8f49adb966b1ebf91830d
안다비 안다비 <dksekqkr1234@naver.com>
2023-09-18 23:47:16 +0900

    fix error

--- Assets/AHN/Scripts/Cook/RawFishTrigger.cs
+++ Assets/AHN/Scripts/Cook/RawFishTrigger.cs
@@ -15,7 +15,7 @@ namespace AHN
         {
             if (other.gameObject.layer == 21)   // 회랑 trigger되면 Resources에서 회와 맞는 초밥을 꺼내와서 SushiManager 자식으로 넣기
             {
-                // TODO : 올려진 이 회의 등급을 받아옴 (회에 스크립트로 등급 점수를 반환하는 함수가 있을 거임. 
+                // 결제 : 올려진 이 회의 등급을 받아옴 (회에 스크립트로 등급 점수를 반환하는 함수가 있을 거임. 
                 //        other.gameObject.GetComponent<>() 로 등급을 받아오고, 이 등급을 변하는 Sushi로 넘겨줘야 함.
                 //        그건 여기 밑에 sushi.gameObject.GetComponent<>()로, Sushi에서 등급을 매개변수로 하는 점수계산 함수를 만들어. 그 함수를 등급을 넣어서 호출하면됨.
                 //        그러면 만들어진 sushi에는 등급이 반영된 점수가 있을 것.
